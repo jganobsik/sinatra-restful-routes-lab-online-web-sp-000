d9944d9c5777280f49237b7f938344979eac9127
Joe Ganobsik <jganobsik@gmail.com>
2019-02-09 22:02:21 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++
@@ -1,9 +0,0 @@
-ActiveRecord::Schema.define(version: 20151117000918) do
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.string "ingredients"
-    t.string "cook_time"
-  end
-
-end
